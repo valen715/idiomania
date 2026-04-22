29d6fd03d0f29d900bb6ad2f3820ed73b242f86f
Valentina Restrepo <vrestrepomontoya@gmail.com>
2026-04-21 22:30:00 -0500

    my first commit

---
+++ src/controlador/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main {
+    public static void main(String[] args) {
+        new vista.Login().setVisible(true);
+    }
+}
